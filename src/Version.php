d569b7c7402c8f66970100eb47b8493332f6953a
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-08-21 14:54:14 -0400

    Prepare for v2.4.2 release. #20

--- src/Version.php
+++ src/Version.php
@@ -29,14 +29,14 @@ class Version
      *
      * @var string
      */
-    const VERSION = '2.4.0';
+    const VERSION = '2.4.1';
 
     /**
      * This package's release date.
      *
      * @var string
      */
-    const RELEASEDATE = 'August 21, 2020';
+    const RELEASEDATE = 'August 22, 2020';
 
     /**
      * This package's name.
